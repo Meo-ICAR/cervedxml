61ead9950b9c78347b4b7e02c376747a315538e5
Meo-ICAR <piergiuseppe.meo@icar.cnr.it>
2026-01-06 15:08:25 +0100

    debug bar

--- config/app.php
+++ config/app.php
@@ -129,6 +129,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
     ],
     'previous_keys' => [
         ...array_filter(
